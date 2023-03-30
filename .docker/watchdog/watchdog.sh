ef90918ee7a12e6f059e8323f478c924190bd012
林柏臣 <daniel.pclin@gmail.com>
2023-03-30 16:59:26 +0800

    update watchdog.sh

--- .docker/watchdog/watchdog.sh
+++ .docker/watchdog/watchdog.sh
@@ -6,12 +6,12 @@ judge_hosts_file="$(realpath "${JUDGE_HOSTS_FILE:-$2}")"
 echo "$(date): Start watching ${problem_storage_root}, notifying ${judge_hosts_file}"
 
 inotifywait -rm "${problem_storage_root}" -e move,create,delete -q | while read -r line; do
-    if [ "$(echo "$line" | cut -d' ' -f3)" != "init.yml" ]; then
+    if [ "$(echo "$line" | cut -d' ' -f3)" != "init.yml" ] && [ "$(echo "$line" | cut -d' ' -f2)" != "MOVED_TO,ISDIR" ]; then
         continue
     fi
     echo "$(date): Update problems [$line]"
     readarray -t judges < "${judge_hosts_file}"
     for judge in "${judges[@]}"; do
-	curl -4 -s -X POST "http://$judge/update/problems" | sed "s/^/$(date) [$judge]: /"';$a\'
+	      curl -4 -s -X POST "http://$judge/update/problems" | sed "s/^/$(date) [$judge]: /"';$a\'
     done
 done
